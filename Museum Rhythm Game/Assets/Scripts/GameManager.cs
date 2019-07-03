bb49c4da01aa08cf8daf20a73e4af16ecad94188
PurePureMilk <gtkpham@gmail.com>
2019-07-03 09:25:06 -0400

    July Update

---
+++ Museum Rhythm Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public AudioSource theMusic;
+    public static bool startPlaying;
+    public static GameManager instance;
+    public Spawner tempoMarkers;
+    public static bool endOfPiece;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //when the game is booted up, the music and game should not start yet
+        if (!startPlaying)
+        {
+            //if a button is pushed, start the beatscroller and music
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                tempoMarkers.hasStarted = true;
+
+            }
+ 
+        }
+        //buggy area; the music doesn't even start
+        else if (!theMusic.isPlaying && Cube.isMusicStarted)
+        {
+            tempoMarkers.hasStarted = false;
+            Destroy(tempoMarkers);
+        }
+
+    }
+
+    public bool NoteMissed()
+    {
+        Debug.Log("Missed Note");
+        return true;
+    }
+}
---
+++ Museum Rhythm Game/Assets/Scripts/Saber.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saber : MonoBehaviour
+{
+    public LayerMask layer;
+    public LayerMask bothLayers;
+    private Vector3 previousPos;
+    public GameManager manager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 1, layer))
+        {
+            if(Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130)
+            {
+                Destroy(hit.transform.gameObject);
+
+                /*when you hit, you start the music, but you also need when you miss
+                if (manager.startPlaying == false)
+                {
+                    manager.theMusic.Play();
+                    manager.startPlaying = true;
+
+                }
+                */
+            }
+        }
+
+        
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1, bothLayers))
+        {
+            if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130)
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+        
+        previousPos = transform.position;
+    }
+}
